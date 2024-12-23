473b7834998eaf33bd4167258c3ffedf832b64a1
priestone <aion97@naver.com>
2024-12-23 16:38:30 +0900

    holy

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,21 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Start from "./pages/start/Start";
+import Detail from "./pages/detail/Detail";
+import Search from "./pages/search/Search";
+import Quiz from "./pages/quiz/Quiz";
+import Result from "./pages/result/Result";
 
 const Router = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Start />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/Detail" element={<Detail />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </HashRouter>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <Wrap>
+      <Quiz></Quiz>
+      <MainText></MainText>
+      <SearchIcon></SearchIcon>
+    </Wrap>
+  );
+};
+
+export default Header;
